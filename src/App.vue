7ef2e719a14fc0af0858322448cfe7cd58f7b842
Nicolò Cavalli <nicolo.cavalli95@gmail.com>
2025-04-17 14:29:36 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Homepage v-for="item in items" :key="item.id" :item='item' />
+</template>
+
+<script setup>
+//=============
+// Import
+//=============
+import Homepage from './components/Homepage.vue';
+
+//=============
+// Consts
+//=============
+const items = [
+  {id: 1, is_premium: false, src:"https://picsum.photos/id/10/200/300"},
+  {id: 2, is_premium: true,  src:"https://picsum.photos/id/20/200/300"},
+  {id: 3, is_premium: true,  src:"https://picsum.photos/id/30/200/300"},
+  {id: 4, is_premium: false, src:"https://picsum.photos/id/40/200/300"},
+  {id: 5, is_premium: false, src:"https://picsum.photos/id/50/200/300"},
+  {id: 6, is_premium: true,  src:"https://picsum.photos/id/60/200/300"}
+];
+
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Homepage.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="img-wrapper" @click="onClick">
+        <img :src="item.src" :alt="item.alt || 'img'">
+        <div v-if="item.is_premium" class="badge">🔒</div>
+    </div>
+</template>
+
+<script setup>
+//=============
+// Props
+//=============
+const props = defineProps({
+  item: {
+    type: Object,
+    default: () => ({})
+  }
+});
+
+
+//=============
+// Functions
+//=============
+function onClick() {
+  alert(props.item.is_premium ? 'premium content' : 'free content');
+}
+
+</script>
+
+<style>
+
+.img-wrapper {
+  margin: 10px;
+  position: relative;
+  cursor: pointer;
+}
+
+.badge {
+  position: absolute;
+  bottom: 10px;
+  right: 6px;
+  border-radius: 4px;
+  background-color: lightyellow;
+  font-size: 20px;
+}
+
+</style>
