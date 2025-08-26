f6fe2877f6113d68c6340ffab1123a5071fd4f47
Nicolò Cavalli <nicolo.cavalli95@gmail.com>
2025-08-26 13:58:22 +0200

    endpoint test

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,26 @@ import Thumbnail from './components/Thumbnail.vue';
 //=============
 // Consts
 //=============
-const items = [
-  {id: 1, is_premium: false, src:"https://picsum.photos/id/10/200/300"},
-  {id: 2, is_premium: true,  src:"https://picsum.photos/id/20/200/300"},
-  {id: 3, is_premium: true,  src:"https://picsum.photos/id/30/200/300"},
-  {id: 4, is_premium: false, src:"https://picsum.photos/id/40/200/300"},
-  {id: 5, is_premium: false, src:"https://picsum.photos/id/50/200/300"},
-  {id: 6, is_premium: true,  src:"https://picsum.photos/id/60/200/300"}
-];
+const items = await getData();
+
+
+//=============
+// Functions
+//=============
+async function getData() {
+  const url = 'https://nestjs-server-0011.onrender.com/images';
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error(error.message);
+  }
+}
 
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
